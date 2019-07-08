1b7f466fc3385612887e29b5c5e1e297c73ee489
phoresis92 <husheart@naver.com>
2019-07-09 00:05:55 +0900

    basic server setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,15 @@
-var webpack = require('webpack');
-
 module.exports = {
-
     entry: './src/index.js',
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
     },
 
-    devServer: {
-        hot: true,
-        inline: true,
-        host: '0.0.0.0',
-        port: 4000,
-        contentBase: __dirname + '/public/',
-    },
-
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['react-hot', 'babel?' + JSON.stringify({
+                loaders: [ 'babel?' + JSON.stringify({
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 })],
@@ -29,7 +18,4 @@ module.exports = {
         ]
     },
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
 };
